ba595fe28db8aa2710f7c424c0c75d6dd68ed207
onlyASimpleUser <60228794+onlyASimpleUser@users.noreply.github.com>
2026-07-18 17:45:28 -0500

    Creación de endpoint y servicio de postulación

---
+++ backend/src/application/dto/application.dto.ts
@@ -0,0 +1,43 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class ApplicationDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly last_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly career: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly university: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly university_semester: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly first_choice_area_id: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly second_choice_area_id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly application_reason: string;
+}
