24d487d6c132b195667d7a6b1182039498b4bec5
onlyASimpleUser <60228794+onlyASimpleUser@users.noreply.github.com>
2026-07-18 22:45:05 -0500

    Configuración de cookies

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Request,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from '../auth/auth.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import express from 'express';
 
 @Controller('admin')
 export class AdminController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body: { username: string; password: string }) {
-    return this.authService.login(body.username, body.password);
+  async login(
+    @Body() body: { username: string; password: string },
+    @Res({ passthrough: true }) response: express.Response,
+  ) {
+    const result = await this.authService.login(body.username, body.password);
+    response.cookie('admin_token', result.access_token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+      maxAge: 2 * 60 * 60 * 1000,
+    });
+
+    return `Success`;
   }
 
   @UseGuards(JwtAuthGuard)
