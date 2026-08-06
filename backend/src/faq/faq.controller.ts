a007859368c1e31611f116dc759fa09255a5d03d
onlyASimpleUser <60228794+mjoacobf@users.noreply.github.com>
2026-08-06 12:49:44 -0500

    fix(faq): Add JwtAuthGuard to POST, PUT, PATCH, and DELETE methods

--- backend/src/faq/faq.controller.ts
+++ backend/src/faq/faq.controller.ts
@@ -7,10 +7,12 @@ import {
   Patch,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { FaqService } from './faq.service';
 import { FaqDto } from './dto/faq.dto';
 import { UpdateOrderDto } from './dto/updateOrder.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('api/faq')
 export class FaqController {
@@ -22,18 +24,21 @@ export class FaqController {
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async postFaq(@Body() dto: FaqDto) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return await this.faqService.postFaq(dto);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async updateFaq(@Param('id') id: number, @Body() dto: FaqDto) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return await this.faqService.updateFaq(id, dto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async deleteFaq(@Param('id') id: number) {
     await this.faqService.deleteFaq(id);
 
@@ -41,6 +46,7 @@ export class FaqController {
   }
 
   @Patch()
+  @UseGuards(JwtAuthGuard)
   async reorder(@Body() dto: UpdateOrderDto[]) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return await this.faqService.reorder(dto);
