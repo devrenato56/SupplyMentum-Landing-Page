20a3efed78ac78eeb7b78f5ec07300b0f4de25c5
devrenato56 <rensto56700@gmail.com>
2026-08-14 01:34:34 -0500

    feat(home): implementacion de secciones en la home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,11 @@ import NetworkBackground from "@/components/home/NetworkBackground";
 import SmoothScroll from "@/components/ui/SmoothScroll";
 import Hero from "@/components/home/Hero";
 import AboutSection from "@/components/home/AboutSection";
+import ChainSection from "@/components/home/ChainSection";
 import MetricsSection from "@/components/home/MetricsSection";
 import Specializations from "@/components/home/Specializations";
+import NovedadesSection from "@/components/home/NovedadesSection";
+import BenefitsSection from "@/components/home/BenefitsSection";
 import ExploreSection from "@/components/home/ExploreSection";
 
 export default function Home() {
@@ -14,8 +17,11 @@ export default function Home() {
       <div className="relative z-[1]">
         <Hero />
         <AboutSection />
+        <ChainSection />
         <MetricsSection />
         <Specializations />
+        <NovedadesSection />
+        <BenefitsSection />
         <ExploreSection />
       </div>
     </div>
